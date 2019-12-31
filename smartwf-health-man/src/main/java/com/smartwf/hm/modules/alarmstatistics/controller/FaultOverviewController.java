8cbe63e08c273cf6e2cc26ea3855ca06ab036b65
changhuang_wan <v_wanchanghuang@163.com>
2019-12-31 17:51:07 +0800

    故障总览

---
+++ smartwf-health-man/src/main/java/com/smartwf/hm/modules/alarmstatistics/controller/FaultOverviewController.java
@@ -0,0 +1,20 @@
+package com.smartwf.hm.modules.alarmstatistics.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Date: 2019-10-25 15:04:26
+ * @Description: 故障总览控制器
+ */
+@RestController
+@RequestMapping("faultoverview")
+@Slf4j
+@Api(description = "故障总览控制器")
+public class FaultOverviewController {
+
+
+}
---
+++ smartwf-health-man/src/main/java/com/smartwf/hm/modules/alarmstatistics/service/impl/FaultOverviewServiceImpl.java
@@ -0,0 +1,17 @@
+package com.smartwf.hm.modules.alarmstatistics.service.impl;
+
+
+import org.springframework.stereotype.Service;
+
+import com.smartwf.hm.modules.alarmstatistics.service.FaultoverviewService;
+
+
+/**
+ * @Date: 2019-11-27 11:25:24
+ * @Description: 故障总览业务层实现
+ */
+@Service
+public class FaultOverviewServiceImpl implements FaultoverviewService {
+
+
+}
