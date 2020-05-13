bf18bb4e5ca275526fbf4f264d4e1f27ac7d3a8f
changhuang_wan <v_wanchanghuang@163.com>
2020-05-13 09:53:14 +0800

    调整参数约束

--- smartwf-health-man/src/main/java/com/smartwf/hm/modules/alarmstatistics/controller/FaultDataController.java
+++ smartwf-health-man/src/main/java/com/smartwf/hm/modules/alarmstatistics/controller/FaultDataController.java
@@ -93,25 +93,25 @@ public class FaultDataController {
     @PostMapping(value = "saveDefect", consumes = "multipart/*", headers = "content-type=multipart/form-data")
     @ApiOperation(value = " 缺陷工单录入接口", notes = " 缺陷工单录入")
     @ApiImplicitParams({
-    	    @ApiImplicitParam(paramType = "query", name = "tenantCode", value = "租户（编码）", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "alarmCode", value = "报警码", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "alarmSource", value = "报警源", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "alarmDescription", value = "报警描述", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "alarmLocation", value = "报警部位", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "alarmLevel", value = "事变等级(0危急 1严重 2一般 3未知)", dataType = "int"),
-    	    @ApiImplicitParam(paramType = "query", name = "incidentType", value = "事变类型(1故障类型 2缺陷类型)", dataType = "int"),
-    	    @ApiImplicitParam(paramType = "query", name = "faultType", value = "报警来源(0故障 1报警 2人工缺陷)", dataType = "int"),
-    	    @ApiImplicitParam(paramType = "query", name = "manufacturers", value = "厂家", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "deviceCode", value = "设备编码", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "deviceName", value = "设备名称", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "windFarm", value = "风场", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "assetNumber", value = "资产编码", dataType = "String"),
-    	    @ApiImplicitParam(paramType = "query", name = "operatingStatus", value = "操作状态(0默认  1重点关注)", dataType = "Integer"),
-    	    @ApiImplicitParam(paramType = "query", name = "startTime", value = "故障起始时间(yyyy-MM-dd HH:mm:ss)", dataType = "Date" ),
-            @ApiImplicitParam(paramType = "query", name = "endTime", value = "故障截止时间(yyyy-MM-dd HH:mm:ss)", dataType = "Date" ),
-            @ApiImplicitParam(paramType = "query", name = "remark", value = "备注", dataType = "String" ),
-            @ApiImplicitParam(paramType = "query", name = "discovererName", value = "发现人姓名（逗号拼接）", dataType = "String" ),
-            @ApiImplicitParam(paramType = "query", name = "discovererId", value = "发现人id（逗号拼接）", dataType = "String" )
+	    	@ApiImplicitParam(paramType = "query", name = "tenantCode", value = "租户（编码）", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "alarmCode", value = "报警码", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "alarmSource", value = "报警源", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "alarmDescription", value = "报警描述", dataType = "String",required = true),
+		    @ApiImplicitParam(paramType = "query", name = "alarmLocation", value = "报警部位", dataType = "String",required = true),
+		    @ApiImplicitParam(paramType = "query", name = "alarmLevel", value = "事变等级(0危急 1严重 2一般 3未知)", dataType = "int",required = true),
+		    @ApiImplicitParam(paramType = "query", name = "incidentType", value = "事变类型(1故障类型 2缺陷类型)", dataType = "int",required = true),
+		    @ApiImplicitParam(paramType = "query", name = "faultType", value = "报警来源(0故障 1报警 2人工缺陷)", dataType = "int",required = true),
+		    @ApiImplicitParam(paramType = "query", name = "manufacturers", value = "厂家", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "deviceCode", value = "设备编码", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "deviceName", value = "设备名称", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "windFarm", value = "风场", dataType = "String"),
+		    @ApiImplicitParam(paramType = "query", name = "assetNumber", value = "资产编码", dataType = "String",required = true),
+		    @ApiImplicitParam(paramType = "query", name = "operatingStatus", value = "操作状态(0默认  1重点关注)", dataType = "Integer"),
+		    @ApiImplicitParam(paramType = "query", name = "startTime", value = "故障起始时间(yyyy-MM-dd HH:mm:ss)", dataType = "Date" ,required = true),
+	        @ApiImplicitParam(paramType = "query", name = "endTime", value = "故障截止时间(yyyy-MM-dd HH:mm:ss)", dataType = "Date" ),
+	        @ApiImplicitParam(paramType = "query", name = "remark", value = "备注", dataType = "String" ),
+	        @ApiImplicitParam(paramType = "query", name = "discovererName", value = "发现人姓名（逗号拼接）", dataType = "String" ),
+	        @ApiImplicitParam(paramType = "query", name = "discovererId", value = "发现人id（逗号拼接）", dataType = "String" )
     })
     public ResponseEntity<Result<?>> saveDefect(DefectVO bean,HttpServletRequest request) {
         try {
