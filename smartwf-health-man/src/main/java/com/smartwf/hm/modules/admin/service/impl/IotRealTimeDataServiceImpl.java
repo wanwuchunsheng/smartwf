7212ad35110c1404e6492eb51b9148888fc076b4
changhuang_wan <v_wanchanghuang@163.com>
2020-12-25 09:42:50 +0800

    测试istio访问mysql-jdbc

--- smartwf-health-man/src/main/java/com/smartwf/hm/modules/admin/service/impl/IotRealTimeDataServiceImpl.java
+++ smartwf-health-man/src/main/java/com/smartwf/hm/modules/admin/service/impl/IotRealTimeDataServiceImpl.java
@@ -1,12 +1,19 @@
 package com.smartwf.hm.modules.admin.service.impl;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.smartwf.common.constant.Constants;
+import com.smartwf.common.service.RedisService;
 import com.smartwf.hm.modules.admin.service.IotRealTimeDataService;
+import com.smartwf.hm.modules.alarmstatistics.dao.AlarmInboxDao;
+import com.smartwf.hm.modules.alarmstatistics.pojo.FaultInformation;
 
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -17,10 +24,16 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class IotRealTimeDataServiceImpl implements IotRealTimeDataService{
+	
+	@Autowired
+	private AlarmInboxDao alarmInboxDao;
+	
+	@Autowired
+	private RedisService redisService;
 
 	/**
 	 * 说明：底层实时数据接入
-	 * @param fault_type 
+	 * @param fault_type
 	 *        0-系统故障  1-监控警告 2-人工提交 3-预警警告
 	 * @author WCH
 	 * */
@@ -29,8 +42,32 @@ public class IotRealTimeDataServiceImpl implements IotRealTimeDataService{
 		try {
 			if(StrUtil.isNotEmpty(value)) {
 				Map<String,Object> map=JSONUtil.parseObj(value);
-				for(Entry<String, Object> m:map.entrySet()) {
-					System.out.println(m.getKey()+"     "+m.getValue());
+				//problemType  0-系统故障  1-监控警告  2-人工提交  3-预警警告
+				if(map.containsKey("problemType")) {
+					for(Entry<String, Object> m:map.entrySet()) {
+						System.out.println(m.getKey()+"     "+m.getValue());
+					}
+					FaultInformation fit=null;
+					if(Constants.ONE==Convert.toInt(map.get("problemType"))) {
+						//告警调用生产中心补全数据
+						Thread.sleep(1500);
+					}else {
+						//预警直接入库
+						fit=new FaultInformation();
+						fit.setAlarmCode("");
+						fit.setAlarmDescription("");
+						fit.setAlarmLevel(1);
+						fit.setAlarmLocation("");
+						fit.setAlarmSource("");
+						fit.setAlarmStatus(0);
+						fit.setAssetNumber("");
+						fit.setCreateTime(new Date());
+						fit.setDeviceCode("");
+						fit.setDeviceName("");
+						fit.setDiscovererId("");
+						fit.setDiscovererName("");
+					    this.alarmInboxDao.insert(fit);
+					}
 				}
 			}
 		} catch (Exception e) {
